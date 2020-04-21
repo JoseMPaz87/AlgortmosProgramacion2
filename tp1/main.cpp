4747ad49fa8c232ea9244f6c325a88eaed89a285
josepazfiuba <jpaz885@gmail.com>
2020-04-20 21:12:55 -0300

    Esta version del tp1 anda bien

--- tp1/main.cpp
+++ tp1/main.cpp
@@ -9,13 +9,15 @@ que para ejecutar se debe escribir "./tp1"
 #include <fstream>
 #include <string>
 #include <ctype.h>
+#include <sstream>
+
 using namespace std;
 
 bool es_numero (string);
 void convertir_enteros_a_romanos (void);
-void cargar_numeros_arabicos (string, int *);
+//void cargar_numeros_arabicos (string, int *);
 
-int numero_decimal [4] = {1,0,3,0};
+
 
 const char * simbolos[4][10] = {
 {"","M","MM","MMM","","","","","",""},
@@ -37,7 +39,7 @@ bool es_numero (string cadena)
 			return false;
 	return true;
 }
-
+/*
 void cargar_numeros_arabicos (int largo,string cadena, int * numero_arabicos)
 {
 	for (int i = 0; cadena[i]; i++)
@@ -45,39 +47,49 @@ void cargar_numeros_arabicos (int largo,string cadena, int * numero_arabicos)
 	
 	}
 }
-
+*/
 
 void convertir_enteros_a_romanos (void)
 {
 	string cadena;
 	ifstream entrada ("valores.txt");
-
-
+	int numero;
+	char * b;
+	int numero_decimal [4];
+	
 	while (entrada >> cadena)
 	{
+		//Valido que sea un numero
 		if (!es_numero (cadena))
 		{	
 			cerr <<  cadena << " no es un número" << endl;
 			continue;
 		}
 		
+		//Valido que no supere los 4 digitos, para asegurarme que no fallara la conversion
 		if (cadena.length() > 4)
 		{
 			cerr << cadena << " número fuera del rango" << ' ' << endl;
 			continue;
 		}	
-			
-		/*for (int i = 0; i < 4; i++)
+		
+		istringstream (cadena) >> numero;
+		//cout << numero << endl;
+		
+		for (int j = 3; j >= 0; j--)
 		{
-			numero_decimal[i] = 0;
-			
-			
-			
+			numero_decimal[j] = numero % 10;
+			numero /= 10;
+		}	
+		
+		for (int i = 0; i < 4; i++)
+		{
+
 			if (numero_decimal[i] == 0)
 				continue;
 			cout << simbolos[i][numero_decimal[i]];
-		}*/
-		//cout << endl;
+		}
+		cout << endl;
 	}
 	
 	
